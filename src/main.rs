cb3136a38671802f72b8ef57ddb65de7ee584181
Arif Rezai <marezai10@gmail.com>
2025-06-05 12:45:52 +0900

    add a comment to main

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@
 ///
 /// This is the best implementation of this program to ever exist.
 
+/// The main function runs when our program starts
 fn main() {
     print("Hello, world!");
+    print("Goodbye, world!");
 }
 
+/// a function that prints a message
 fn print(m: &str) {
     println!("{m}");
 }
